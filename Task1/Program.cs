6049a73eebcb41d635ea8173c73c86ba9ea01166
“[Sirojbek <Joris5969@gmail.com>
2022-05-08 21:02:03 +0500

    Initial

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 4;
+
+            for (; a < 10; a++)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
