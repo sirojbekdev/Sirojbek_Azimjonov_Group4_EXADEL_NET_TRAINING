2baa61b93259fd37cddf6e60a3d1db296c1bbeed
“[Sirojbek <Joris5969@gmail.com>
2022-05-12 19:54:24 +0500

    Initial

---
+++ Task2.1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace Task2._1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value for a between 0 and 5");
+            int a = Convert.ToInt32(Console.ReadLine());
+            bool aFlag = Enumerable.Range(0, 6).Contains(a);
+            while (!aFlag)
+            {
+                if (!Enumerable.Range(0, 6).Contains(a))
+                {
+                    Console.WriteLine("Please, enter value for a between 0 and 5");
+                    a = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+                    aFlag=true;
+                }
+            }
+            
+            double aFactorial = 1;
+            for (int x = 1; x <= a; x++)
+            {
+                aFactorial *= x;
+            }
+
+            Console.WriteLine("Enter value for b between 0 and 100");
+            int b = Convert.ToInt32(Console.ReadLine());
+            bool bFlag = Enumerable.Range(0, 101).Contains(b);
+            while (!bFlag)
+            {
+                if (!Enumerable.Range(0, 101).Contains(b))
+                {
+                    Console.WriteLine("Please, enter value for b between 0 and 100");
+                    b = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+                    bFlag=true;
+                }
+            }
+           
+            double bLogorithm = Math.Log(b);
+            var aValue = Math.Round(aFactorial*0.05,2);
+            var bValue = Math.Round(bLogorithm,2);
+
+            if (aValue > bValue)
+            {
+                Console.WriteLine($"Number 'a' = {aValue} and more than number 'b' = {bValue}");
+            }
+            else if(aValue < bValue)
+            {
+                Console.WriteLine($"Number 'a' = {aValue} and less than number 'b' = {bValue}");
+            }
+            else
+            {
+                Console.WriteLine($"Number 'a' = {aValue} and equal to number 'b' = {bValue}");
+            }
+        }
+    }
+}
---
+++ Task2.2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Task2._2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string file1 = System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory+"Files/file1.txt");
+            string file2 = System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory+"Files/file2.txt");
+            string file3 = System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory+"Files/file3.txt");
+            string[] files = new string[]
+            {
+                file1,file2,file3
+            };
+            //Console.WriteLine(text);
+            //List<string> files = new List<string>()
+            //{
+            //    " Ak! NjodH22# pknGhb** gggruuee$67",
+            //    "fkrfji rgjir rmgjK",
+            //    "1111 1$%^&*"
+            //};
+
+            foreach (string file in files)
+            {
+
+                Console.WriteLine(file);
+            }
+
+            for (int file = 0; file < files.Length; file++)
+            {
+                string[] temp = Regex.Replace(files[file], @"[^a-z A-Z]", "").Split(' ').Reverse().ToArray();
+
+                for (int word = 0; word < temp.Length; word++)
+                {
+                    for (int c = 0; c < temp[word].Length; c++)
+                    {
+                        if (char.IsUpper(temp[word][c]) && c != 0)
+                        {
+                            var index = temp[word].IndexOf(temp[word].FirstOrDefault(i => char.IsLower(i)));
+                            temp[word] = temp[word].Remove(c, 1).Insert(index, temp[word][c].ToString());
+                        }
+                    }
+                }
+                files[file] = string.Join(" ", temp);
+            }
+            files = files.OrderBy(x=>x.Length).ToArray();
+            Console.WriteLine("After Changes");
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+    }
+}
