a624be2f4094c6b73c4f58316c2d4d364bfc5dc0
“[Sirojbek <Joris5969@gmail.com>
2022-05-15 01:47:14 +0500

    Fix 2nd review

--- Task2/Task2.1/Program.cs
+++ Task2/Task2.1/Program.cs
@@ -9,8 +9,20 @@ namespace Task2._1
         {
             Console.WriteLine("Enter value for a between 0 and 5");
             int a = Convert.ToInt32(Console.ReadLine());
+            bool aFlag = Enumerable.Range(0, 6).Contains(a);
+            while (!aFlag)
+            {
+                if (!Enumerable.Range(0, 6).Contains(a))
+                {
+                    Console.WriteLine("Please, enter proper value for a between 0 and 5");
+                    a = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+                    aFlag = true;
+                }
+            }
 
-            
             double aFactorial = 1;
             for (int x = 1; x <= a; x++)
             {
@@ -19,8 +31,20 @@ namespace Task2._1
 
             Console.WriteLine("Enter value for b between 0 and 100");
             int b = Convert.ToInt32(Console.ReadLine());
+            bool bFlag = Enumerable.Range(0, 101).Contains(b);
+            while (!bFlag)
+            {
+                if (!Enumerable.Range(0, 101).Contains(b))
+                {
+                    Console.WriteLine("Please, enter proper value for b between 0 and 100");
+                    b = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+                    bFlag = true;
+                }
+            }
 
-           
             double bLogorithm = Math.Log(b);
             var aValue = Math.Round(aFactorial*0.05,2);
             var bValue = Math.Round(bLogorithm,2);
