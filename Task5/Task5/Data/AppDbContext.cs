92db81ba7f0341227711d03c9a7de8c8f512014a
“[Sirojbek <Joris5969@gmail.com>
2022-05-22 03:26:04 +0500

    Initial

---
+++ Task5/Task5/Data/AppDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task5.Models;
+
+namespace Task5.Data
+{
+    public class AppDbContext :DbContext
+    {
+        private readonly IConfiguration _IConfiguration;
+        private readonly string _ConnectionString;
+        public AppDbContext()
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                       .SetBasePath(Directory.GetCurrentDirectory())
+                       .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            _IConfiguration = builder.Build();
+            _ConnectionString = _IConfiguration.GetConnectionString("default");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_ConnectionString);
+        }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<ClassSubject> ClassSubjects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ClassSubject>()
+                .HasKey(cs => new { cs.ClassId, cs.SubjectId });
+            modelBuilder.Entity<ClassSubject>()
+                .HasOne(cs => cs.Class)
+                .WithMany(c => c.ClassSubjects)
+                .HasForeignKey(bc => bc.ClassId);
+            modelBuilder.Entity<ClassSubject>()
+                .HasOne(cs => cs.Subject)
+                .WithMany(s => s.ClassSubjects)
+                .HasForeignKey(cs => cs.SubjectId);
+        }
+    }
+}
---
+++ Task5/Task5/Models/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Address { get; set; }
+        public int ClassId { get; set; }
+        public Class Class { get; set; }
+    }
+}
