4ec50787d34a66112bf47e306210747776958752
“[Sirojbek <Joris5969@gmail.com>
2022-05-24 01:58:12 +0500

    Initial 6.1

---
+++ Task6/Task6.1/Models/ClassSubject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6._1.Models
+{
+    public class ClassSubject
+    {
+        public int ClassId { get; set; }
+        public Class Class { get; set; }
+        public int SubjectId { get; set; }
+        public Subject Subject { get; set; }
+    }
+}
---
+++ Task6/Task6.1/Program.cs
@@ -0,0 +1,100 @@
+﻿using Task6._1.Data;
+using Task6._1.Models;
+
+var context = new AppDbContext();
+
+async Task CreateAsync(string name)
+{
+    if (name != null)
+    {
+        Subject subject = new()
+        {
+            Name = name
+        };
+        context.Subjects.Add(subject);
+        await context.SaveChangesAsync();
+        Console.WriteLine("Subject created successfully");
+    }
+    else
+    {
+        Console.WriteLine("Wrong input");
+    }
+}
+async Task UpdateAsync(int id, string newName)
+{
+    var subject = await context.Subjects.FindAsync(id);
+    if (subject != null)
+    {
+        subject.Name = newName;
+        await context.SaveChangesAsync();
+        Console.WriteLine("Subject updated successfully");
+    }
+    else
+    {
+        Console.WriteLine("Wrong input or data does not exist");
+    }
+}
+async Task DeleteAsync(int id)
+{
+    var subject = await context.Subjects.FindAsync(id);
+    if (subject != null)
+    {
+        context.Subjects.Remove(subject);
+        await context.SaveChangesAsync();
+        Console.WriteLine("Subject deleted successfully");
+    }
+    else
+    {
+        Console.WriteLine("Wrong input or data does not exist");
+    }
+}
+async Task GetByIdAsync(int id)
+{
+    var subject = await context.Subjects.FindAsync(id);
+    if (subject != null)
+    {
+        Console.WriteLine($"Subject id: {subject.Id}, Subject name: {subject.Name}");
+    }
+    else
+    {
+        Console.WriteLine("Wrong input or data does not exist");
+    }
+}
+
+Console.WriteLine("1.Create subject \n2.Update subject by ID\n3.Delete subject by ID\n4.Read subject by ID\n5.Exit");
+var key = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(key);
+if (key == 1)
+{
+    Console.WriteLine("Enter name for new subject");
+    var name = Console.ReadLine();
+    await CreateAsync(name);
+}
+else if (key == 2)
+{
+    Console.WriteLine("Enter the id of subject");
+    var id = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter new name for the subject");
+    var newName = Console.ReadLine();
+    await UpdateAsync(id, newName);
+}
+else if (key == 3)
+{
+    Console.WriteLine("Enter the id of subject");
+    var id = Convert.ToInt32(Console.ReadLine());
+    await DeleteAsync(id);
+}
+else if (key == 4)
+{
+    Console.WriteLine("Enter the id of subject");
+    var id = Convert.ToInt32(Console.ReadLine());
+    await GetByIdAsync(id);
+}
+else if (key == 5)
+{
+    Environment.Exit(0);
+}
+else
+{
+    Console.WriteLine("Wrong input");
+}
