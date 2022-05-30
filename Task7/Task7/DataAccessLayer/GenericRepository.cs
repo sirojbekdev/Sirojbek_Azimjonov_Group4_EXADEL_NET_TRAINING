5197803dc44039200a5fdcd69389f086297c278f
“[Sirojbek <Joris5969@gmail.com>
2022-05-30 18:18:50 +0500

    Initial

---
+++ Task7/Task7/DataAccessLayer/GenericRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Task7.Data;
+
+namespace Task7.DataAccessLayer
+{
+    public abstract class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        internal AppDbContext _context;
+        internal DbSet<TEntity> dbSet;
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            dbSet = context.Set<TEntity>();
+        }
+
+        public void Delete(TEntity entityToDelete)
+        {
+            dbSet.Remove(entityToDelete);
+            Save();
+        }
+
+        public void Delete(object id)
+        {
+            TEntity entity = dbSet.Find(id);
+            Delete(entity);
+        }
+
+        public TEntity GetByID(object id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void Insert(TEntity entity)
+        {
+            dbSet.Add(entity);
+            Save();
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(TEntity entityToUpdate)
+        {
+            dbSet.Attach(entityToUpdate);
+            _context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+    }
+}
---
+++ Task7/Task7/Program.cs
@@ -0,0 +1,11 @@
+﻿using Task7.Data;
+using Task7.Models;
+
+AppDbContext dbContext = new();
+
+List<Subject> subjects = dbContext.Subjects.ToList();
+
+foreach(Subject subject in subjects)
+{
+    Console.WriteLine(subject.Name);
+}
