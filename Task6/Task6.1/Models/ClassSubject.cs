ed0a2aa4910a6befc4ecf08e29ff7dca9ace644a
“[Sirojbek <Joris5969@gmail.com>
2022-05-31 02:30:22 +0500

    removed redundant usings

--- Task6/Task6.1/Models/ClassSubject.cs
+++ Task6/Task6.1/Models/ClassSubject.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Task6._1.Models
+﻿namespace Task6._1.Models
 {
     public class ClassSubject
     {
--- Task6/Task6.2/Methods.cs
+++ Task6/Task6.2/Methods.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Task6._2
+﻿namespace Task6._2
 {
     public class Methods
     {
