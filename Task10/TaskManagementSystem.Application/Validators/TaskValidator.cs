05efdb0a9dc48b5a2501df9a865a31ba365e8336
“[Sirojbek <Joris5969@gmail.com>
2022-06-14 14:21:34 +0500

    Initial

---
+++ Task10/TaskManagementSystem.Application/Validators/TaskValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using TaskManagementSystem.Domain.Entities;
+
+namespace TaskManagementSystem.Application.Validators
+{
+    public class TaskValidator : AbstractValidator<TaskItem>
+    {
+        public TaskValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.Name).NotNull().MaximumLength(50);
+            RuleFor(x => x.Description).MaximumLength(2000);
+            RuleFor(x => x.Status).NotNull();
+            RuleFor(x => x.CreatorId).NotNull();
+        }
+    }
+}
---
+++ Task10/TaskManagementSystem.Application/Validators/UserValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using TaskManagementSystem.Domain.Entities;
+
+namespace TaskManagementSystem.Application.Validators
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.FullName).NotNull().MaximumLength(50);
+            RuleFor(x => x.RoleId).NotNull();
+            RuleFor(x => x.Email).NotNull().MaximumLength(100).EmailAddress();
+            RuleFor(x => x.Password).NotNull().Length(7, 20);
+        }
+    }
+}
